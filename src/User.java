b7ee5b05361361cced0db6ef8823811809d9b3bc
GustavoObara <luizobara@gmail.com>
2023-10-06 11:30:23 -0300

    Caixa branca :: Etapa 2

--- src/User.java
+++ src/User.java
@@ -4,29 +4,29 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class User {
-    public Connection conectarBD() {
-        Connection conn = null;
-        try{
-            Class.forName("com.mysql.Driver.Manager").newInstance();
-            String url = "jdbc:mysql://127.0.0.1/test?user=lopes&password=123";
-            conn = DriverManager.getConnection(url);
-        }catch (Exception e) { }
-        return conn; }
+    /*3*/public Connection conectarBD() {
+        /*4*/Connection conn = null;
+        /*5*/try{
+            /*5*/Class.forName("com.mysql.Driver.Manager").newInstance();
+            /*5*/String url = "jdbc:mysql://127.0.0.1/test?user=lopes&password=123";
+            /*5*/conn = DriverManager.getConnection(url);
+        }/*7*/catch (Exception e) { }
+        /*6*/return conn; }
     public String nome="";
     public boolean result = false;
     public boolean verificarUsuario(String login, String senha) {
-        String sql = "";
-        Connection conn = conectarBD();
+        /*1*/String sql = "";
+        /*2*/Connection conn = conectarBD();
         // INSTRUÇÃO SQL
         sql += "select nome from usuarios ";
         sql += "where login = " + "'" + login + "'";
-        sql += " and senha = " + "'" + senha + "'";
-        try{
-            Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery(sql);
-            if (rs.next()) {
-                result = true;
-                nome = rs.getString("nome"); }
-        } catch (Exception e) { }
-        return result;}
+        sql += " and senha = " + "'" + senha + "'";/*8*/
+        /*9*/try{
+            /*9*/Statement st = conn.createStatement();
+            /*9*/ResultSet rs = st.executeQuery(sql);
+            /*10*/if (rs.next()) {
+                /*11*/result = true;
+                /*11*/nome = rs.getString("nome"); }
+        } /*12*/catch (Exception e) { }
+        /*13*/return result;}
 }
